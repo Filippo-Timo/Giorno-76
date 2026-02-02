cefae3232800787c507b7dbbbead18ce4f7d9b4d
Filippo-Timo <filtimo2002@gmail.com>
2026-02-02 14:13:40 +0100

    Initial commit

---
+++ src/test/java/filippotimo/Giorno_76/Giorno76ApplicationTests.java
@@ -0,0 +1,13 @@
+package filippotimo.Giorno_76;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Giorno76ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
